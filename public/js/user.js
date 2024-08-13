6593a15d6c4c4913f6b3cbb7499d76d425531a97
hweltzien <hmweltzien@gmail.com>
2024-08-13 10:33:26 -0700

    fetch requests added to user.js

--- public/js/user.js
+++ public/js/user.js
@@ -1,52 +1,84 @@
 //reference to the DOM element
-const formEl = document.querySelector('#myForm');
-const usernameInput = document.querySelector('#username');
-const titleInput = document.querySelector('#title');
-const contentInput = document.querySelector('#content');
-const msgDiv = document.querySelector('#msg');
-const submitBtn = document.querySelector('.submitBtn');
-//all blog posts are saved in local storage and saved to an array
-const allBlogPost = JSON.parse(localStorage.getItem('blogPosts')) || [];
+const formEl = document.querySelector('.user-form');
 
+const titleInput = document.querySelector('.title');
+const contentInput = document.querySelector('.content');
+const imageInput = document.querySelector('.image');
+
+const submitBtn = document.querySelector('.submit');
+
+//all blog posts are saved in db
+
+
+const allBlogPost = fetch ("/api/user", {
+    method: "POST",
+    body: JSON.stringify({ title, content, image }),
+    headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+    console.log("response", response);
+    document.location.replace("/user");
+  } else {
+    alert("Failed to sign up.");
+  };
 //display blog post 
 //  renderBlogPost();
 
-//changing text content to a message depending on conditions
- function displayMessage(type, message) {
-    msgDiv.textContent = message;
-    msgDiv.setAttribute('class', type);
+//changing text content depending on conditions
+ function displayContent(type, content) {
+    contentDiv.textContent = content;
+    contentDiv.setAttribute('class', type);
  }
-//calling items from local storage
-function renderBlogPost() {
-    const username = localStorage.getItem('username');
-    const title = localStorage.getItem('title');
-    const content = localStorage.getItem('content');
 
-    usernameInput.textContent = username;
-    titleInput.textContent = title;
-    contentInput.textContent = content;
-}
+ //calling items from db
+ function renderBlogPost() {
+    let blogPosts = fetch ("/api/posts");
+    blogPosts.forEach((blogPost) => {
+        const blogPostDiv = document.createElement('div');
+        blogPostDiv.setAttribute('class', 'blog-post');
+        blogPostDiv.innerHTML = `
+            <h2>${blogPost.title}</h2>
+            <p>${blogPost.content}</p>
+            <p>${response.username}</p>
+        `;
+        blogPostDiv.appendChild(blogPostDiv);
+    });
+ }
+// function renderBlogPost ({fetch ("/api/user"), {
+    
+      
+      
+
+    
+//     titleInput.textContent = title;
+//     contentInput.textContent = content;
+// }
 //checking that user input is provided on clicking "submit"
 formEl.addEventListener('submit', function (event) {
     event.preventDefault();
-    const username = usernameInput.value;
+    
     const title = titleInput.value;
     const content = contentInput.value;
-console.log(username, title,content,"Add new post")
-    if (username === '') {
-        // displayMessage('error', 'Username cannot be blank');
-        alert("Username cannot be blank")
-        return;
-    } else if (title === '') {
+console.log(title,content,"Add new post")
+    
+    if (title === '') {
         //displayMessage('error', 'Please provide title');
         alert("Please provide title")
         return;
     } else {
         displayMessage('success', 'Posted!');
        
-        localStorage.setItem('username', username);
-        localStorage.setItem('title', title);
-        localStorage.setItem('content', content);
+        const allBlogPost = fetch ("/api/user", {
+            method: "POST",
+            body: JSON.stringify({ title, content, image }),
+            headers: { "Content-Type": "application/json" },
+          });
+          if (response.ok) {
+            console.log("response", response);
+            document.location.replace("/user");
+          } else {
+            alert("Failed to sign up.");
+          };
         renderBlogPost();
     }
     
